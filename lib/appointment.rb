6e91248c788048f0aed7f90fab8bd8adaf3c3016
Marques Avery <averymarques4@gmail.com>
2020-08-28 19:17:27 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,9 +1,13 @@
 class Appointment
-  attr_accessor
+  attr_accessor :date, :patient, :doctor
 
   @@all = []
 
   def initialize(date, patient, doctor)
+    @date = date
+    @pateint = patient
+    @doctor = doctor
+    @@all << self
 
   def self.all
     @@all
