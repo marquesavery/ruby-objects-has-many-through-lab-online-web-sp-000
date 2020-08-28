0697499c24ba92518fe7dd1bdce7ae5b5c3e48fb
Marques Avery <averymarques4@gmail.com>
2020-08-28 19:16:35 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -9,7 +9,7 @@ class Patient
   end
 
   def new_appointment(date, doctor)
-    self.appointment = Appointment.new(date, self, doctor)
+    Appointment.new(date, self, doctor)
   end
 
   def self.all
